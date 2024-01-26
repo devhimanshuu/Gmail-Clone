39c8b87e061eb8968c1be486dcf7aa5e261d4058
devhimanshuu <devhimanshuu@gmail.com>
2024-01-27 03:25:04 +0530

    App

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Signin from "./Components/Signin";
+import React from "react";
+function App() {
+  return (
+    <div>
+      <Signin />
+    </div>
+  );
+}
+
+export default App;
