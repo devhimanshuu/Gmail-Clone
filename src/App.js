6f5cfb34944a335d8ea46e62d8003259647d5412
devhimanshuu <devhimanshuu@gmail.com>
2024-01-28 03:51:16 +0530

    app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Navbar from "./Components/Navbar";
 import Signin from "./Components/Signin";
 import React from "react";
 function App() {
   return (
     <div>
       <Signin />
+      <Navbar />
     </div>
   );
 }
