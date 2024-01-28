863723bf2c744bf8b5cd5db13a32da1274df5f3a
devhimanshuu <devhimanshuu@gmail.com>
2024-01-28 16:30:49 +0530

    add responsiveness

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -8,30 +8,92 @@ import Typography from "@mui/material/Typography";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
 import Gmail from "../images/gmail.png";
-
+import { Avatar, Grid } from "@mui/material";
+import lens from "../images/lens.png";
+import { auth } from "../Firebase/setup";
 export default function Navbar() {
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static" sx={{ backgroundColor: "#F9F9F9" }}>
-        <Toolbar variant="dense">
-          <IconButton
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2, color: "3C3C3C" }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <img src={Gmail} style={{ width: "2vw" }} />
-          <Typography
-            sx={{ color: "#3C3C3C", marginLeft: "10px" }}
-            variant="h6"
-            component="div"
-          >
-            Gmail
-          </Typography>
-        </Toolbar>
-      </AppBar>
-    </Box>
+    <Grid container>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar
+          elevation={0}
+          position="static"
+          sx={{
+            backgroundColor: "#F9F9F9",
+            minHeight: "5vw",
+            minWidth: "100vw",
+            paddingTop: "7px",
+            paddingRight: "30px",
+          }}
+        >
+          <Toolbar variant="dense">
+            <Grid item xs={2}>
+              <div style={{ display: "flex", alignItems: "center" }}>
+                <IconButton
+                  edge="start"
+                  color="inherit"
+                  aria-label="menu"
+                  sx={{ mr: "2vw", color: "#3C3C3C" }}
+                >
+                  <MenuIcon sx={{ width: "2vw" }} />
+                </IconButton>
+                <img src={Gmail} style={{ width: "3vw" }} />
+                <Typography
+                  sx={{
+                    color: "#3C3C3C",
+                    marginLeft: "3vw",
+                    fontSize: "1.6vw",
+                  }}
+                  variant="h6"
+                  component="div"
+                >
+                  Gmail
+                </Typography>
+              </div>
+            </Grid>
+            <Grid item xs={9}>
+              <div
+                style={{
+                  marginLeft: "3vw",
+                  display: "flex",
+                  alignItems: "center",
+                  borderRadius: "40px",
+                  backgroundColor: "#E4EFFA",
+                  width: "55vw",
+                  height: "3.7vw",
+                }}
+              >
+                <img
+                  src={lens}
+                  style={{
+                    width: "1.3vw",
+                    height: "1.3vw",
+                    alignItems: "center",
+                    marginLeft: "15px",
+                  }}
+                  alt="lens_icon"
+                />
+                <input
+                  placeholder="Search mail"
+                  style={{
+                    marginLeft: "3vw",
+                    height: "3vw",
+                    width: "45vw",
+                    backgroundColor: "#E4EFFA",
+                    border: "none",
+                  }}
+                />
+              </div>
+            </Grid>
+            <Grid item xs={1}>
+              <Avatar
+                sx={{ marginLeft: "7.5vw", width: "3vw", height: "3vw" }}
+                src={auth.currentUser?.photoURL}
+              />
+            </Grid>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    </Grid>
   );
 }
