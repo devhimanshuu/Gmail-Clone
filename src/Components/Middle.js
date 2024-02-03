d8429cf4421d9044f9abb41b9ad4c3a1669093bd
devhimanshuu <devhimanshuu@gmail.com>
2024-02-04 02:58:48 +0530

    Middle

--- src/Components/Middle.js
+++ src/Components/Middle.js
@@ -1,9 +1,39 @@
 import { Paper, List, ListItem } from "@mui/material";
 import React from "react";
 import star from "../images/starIcon.png";
+import refresh from "../images/refreshIcon.png";
 function Middle() {
   return (
     <div style={{ marginLeft: "4.3vw", width: "75vw" }}>
+      <img
+        src={refresh}
+        alt=""
+        style={{ marginLeft: "1.5vw", marginTop: "1.2vw" }}
+      />
+      <Paper
+        elevation={0}
+        style={{
+          borderBottom: "1px solid lightgrey",
+          borderTop: "1px solid lightgrey",
+          marginTop: "1.5vw",
+        }}
+      >
+        <List>
+          <ListItem>
+            <img
+              src={star}
+              style={{ width: "1.4vw", height: "1.4vw" }}
+              alt=""
+            />
+            <span style={{ marginLeft: "1.2vw", fontWeight: "500" }}>
+              hhh
+              <span style={{ marginLeft: "12vw", fontWeight: "200" }}>
+                hghjg
+              </span>
+            </span>
+          </ListItem>
+        </List>
+      </Paper>
       <Paper elevation={0} style={{ border: "1px solid lightgrey" }}>
         <List>
           <ListItem>
@@ -12,9 +42,198 @@ function Middle() {
               style={{ width: "1.4vw", height: "1.4vw" }}
               alt=""
             />
-            <span>
+            <span style={{ marginLeft: "1.2vw", fontWeight: "500" }}>
               hhh
-              <span>hghjg</span>
+              <span style={{ marginLeft: "12vw", fontWeight: "200" }}>
+                hghjg
+              </span>
+            </span>
+          </ListItem>
+        </List>
+      </Paper>
+      <Paper elevation={0} style={{ border: "1px solid lightgrey" }}>
+        <List>
+          <ListItem>
+            <img
+              src={star}
+              style={{ width: "1.4vw", height: "1.4vw" }}
+              alt=""
+            />
+            <span style={{ marginLeft: "1.2vw", fontWeight: "500" }}>
+              hhh
+              <span style={{ marginLeft: "12vw", fontWeight: "200" }}>
+                hghjg
+              </span>
+            </span>
+          </ListItem>
+        </List>
+      </Paper>
+      <Paper elevation={0} style={{ border: "1px solid lightgrey" }}>
+        <List>
+          <ListItem>
+            <img
+              src={star}
+              style={{ width: "1.4vw", height: "1.4vw" }}
+              alt=""
+            />
+            <span style={{ marginLeft: "1.2vw", fontWeight: "500" }}>
+              hhh
+              <span style={{ marginLeft: "12vw", fontWeight: "200" }}>
+                hghjg
+              </span>
+            </span>
+          </ListItem>
+        </List>
+      </Paper>
+      <Paper elevation={0} style={{ border: "1px solid lightgrey" }}>
+        <List>
+          <ListItem>
+            <img
+              src={star}
+              style={{ width: "1.4vw", height: "1.4vw" }}
+              alt=""
+            />
+            <span style={{ marginLeft: "1.2vw", fontWeight: "500" }}>
+              hhh
+              <span style={{ marginLeft: "12vw", fontWeight: "200" }}>
+                hghjg
+              </span>
+            </span>
+          </ListItem>
+        </List>
+      </Paper>
+      <Paper elevation={0} style={{ border: "1px solid lightgrey" }}>
+        <List>
+          <ListItem>
+            <img
+              src={star}
+              style={{ width: "1.4vw", height: "1.4vw" }}
+              alt=""
+            />
+            <span style={{ marginLeft: "1.2vw", fontWeight: "500" }}>
+              hhh
+              <span style={{ marginLeft: "12vw", fontWeight: "200" }}>
+                hghjg
+              </span>
+            </span>
+          </ListItem>
+        </List>
+      </Paper>
+      <Paper elevation={0} style={{ border: "1px solid lightgrey" }}>
+        <List>
+          <ListItem>
+            <img
+              src={star}
+              style={{ width: "1.4vw", height: "1.4vw" }}
+              alt=""
+            />
+            <span style={{ marginLeft: "1.2vw", fontWeight: "500" }}>
+              hhh
+              <span style={{ marginLeft: "12vw", fontWeight: "200" }}>
+                hghjg
+              </span>
+            </span>
+          </ListItem>
+        </List>
+      </Paper>
+      <Paper elevation={0} style={{ border: "1px solid lightgrey" }}>
+        <List>
+          <ListItem>
+            <img
+              src={star}
+              style={{ width: "1.4vw", height: "1.4vw" }}
+              alt=""
+            />
+            <span style={{ marginLeft: "1.2vw", fontWeight: "500" }}>
+              hhh
+              <span style={{ marginLeft: "12vw", fontWeight: "200" }}>
+                hghjg
+              </span>
+            </span>
+          </ListItem>
+        </List>
+      </Paper>
+      <Paper elevation={0} style={{ border: "1px solid lightgrey" }}>
+        <List>
+          <ListItem>
+            <img
+              src={star}
+              style={{ width: "1.4vw", height: "1.4vw" }}
+              alt=""
+            />
+            <span style={{ marginLeft: "1.2vw", fontWeight: "500" }}>
+              hhh
+              <span style={{ marginLeft: "12vw", fontWeight: "200" }}>
+                hghjg
+              </span>
+            </span>
+          </ListItem>
+        </List>
+      </Paper>
+      <Paper elevation={0} style={{ border: "1px solid lightgrey" }}>
+        <List>
+          <ListItem>
+            <img
+              src={star}
+              style={{ width: "1.4vw", height: "1.4vw" }}
+              alt=""
+            />
+            <span style={{ marginLeft: "1.2vw", fontWeight: "500" }}>
+              hhh
+              <span style={{ marginLeft: "12vw", fontWeight: "200" }}>
+                hghjg
+              </span>
+            </span>
+          </ListItem>
+        </List>
+      </Paper>
+      <Paper elevation={0} style={{ border: "1px solid lightgrey" }}>
+        <List>
+          <ListItem>
+            <img
+              src={star}
+              style={{ width: "1.4vw", height: "1.4vw" }}
+              alt=""
+            />
+            <span style={{ marginLeft: "1.2vw", fontWeight: "500" }}>
+              hhh
+              <span style={{ marginLeft: "12vw", fontWeight: "200" }}>
+                hghjg
+              </span>
+            </span>
+          </ListItem>
+        </List>
+      </Paper>
+      <Paper elevation={0} style={{ border: "1px solid lightgrey" }}>
+        <List>
+          <ListItem>
+            <img
+              src={star}
+              style={{ width: "1.4vw", height: "1.4vw" }}
+              alt=""
+            />
+            <span style={{ marginLeft: "1.2vw", fontWeight: "500" }}>
+              hhh
+              <span style={{ marginLeft: "12vw", fontWeight: "200" }}>
+                hghjg
+              </span>
+            </span>
+          </ListItem>
+        </List>
+      </Paper>
+      <Paper elevation={0} style={{ border: "1px solid lightgrey" }}>
+        <List>
+          <ListItem>
+            <img
+              src={star}
+              style={{ width: "1.4vw", height: "1.4vw" }}
+              alt=""
+            />
+            <span style={{ marginLeft: "1.2vw", fontWeight: "500" }}>
+              hhh
+              <span style={{ marginLeft: "12vw", fontWeight: "200" }}>
+                hghjg
+              </span>
             </span>
           </ListItem>
         </List>
